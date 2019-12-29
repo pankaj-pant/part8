ebc53490903a241bc66fb5c9efad0ed4e65a9e30
pankaj-pant <pankajpant89@gmail.com>
2019-12-29 20:19:41 +0200

    8.8

---
+++ library-frontend/src/App.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react'
+import Authors from './components/Authors'
+import Books from './components/Books'
+import NewBook from './components/NewBook'
+import { gql } from 'apollo-boost'
+import { Query } from 'react-apollo'
+
+
+const ALL_AUTHORS = gql`
+{
+  allAuthors  {
+    name,
+    born,
+    bookCount
+  }
+}
+`
+
+const App = () => {
+  const [page, setPage] = useState('authors')
+  return <Query query={ALL_AUTHORS}>
+    {(result) => { 
+      if ( result.loading ) {
+        return <div>loading...</div>
+      }
+  return (
+    <div>
+      <div>
+        <button onClick={() => setPage('authors')}>authors</button>
+        <button onClick={() => setPage('books')}>books</button>
+        <button onClick={() => setPage('add')}>add book</button>
+      </div>
+  
+      <Authors
+        show={page === 'authors'}
+        authors={result.data.allAuthors}
+      />
+
+      <Books
+        show={page === 'books'}
+      />
+
+      <NewBook
+        show={page === 'add'}
+      />
+
+    </div>
+  )
+}}
+</Query>
+}
+
+export default App
